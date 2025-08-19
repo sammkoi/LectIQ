b96279f6dfa096661e68841dbc7f29ddaeaf2294
Roderick Lan <rodericklan03@gmail.com>
2025-08-19 15:02:13 -0600

    started front page

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
         className={`${geist.variable} ${kameron.variable} antialiased`}
       >
         <Navbar></Navbar>
-        <main className="flex m-4 ml-48 mr-48 border-black border-1">
+        <main className="flex m-4 ml-24 mr-24 md:m-8 md:ml-48 md:mr-48">
           {children}
         </main>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
 import Image from "next/image";
 
+function Head() {
+  return (
+    <div className="flex flex-row text-(--text) justify-between w-[100%]">
+      <section>
+        <h1 className="text-6xl font-['Kameron']">
+        ChemDB Search
+        </h1>
+        <p className="mt-2 font-['Geist'] text-sm font-medium max-w-lg">
+          Database search engine for binding data for carbohydrate libraries and compounds.
+          This is a living database, with data primarily sourced from <a>these papers</a>.
+        </p>
+      </section>
+      <section className="text-(--text-dim) font-medium text-sm">
+        <p>University of Alberta</p>
+      </section>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
-    <div>hello world</div>
+    <Head />
   );
 }
