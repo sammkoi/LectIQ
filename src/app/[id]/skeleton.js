2883e780375130e05d40c6f7422285155cf1de07
Roderick Lan <rodericklan03@gmail.com>
2025-09-28 12:50:27 -0600

    skeleton update, starting anim

--- src/app/[id]/skeleton.js
+++ src/app/[id]/skeleton.js
@@ -1,9 +1,15 @@
+import { motion } from "motion/react";
+
 
 export default function LectinContentSkeleton() {
   return (
     <div className="flex flex-col w-full h-full gap-4">
-      <div className="w-[15rem] bg-(--search-bar-color) h-[2.5rem] rounded-lg" ></div>
-      <div className="w-full bg-(--search-bar-color) rounded-lg h-[50vh]" ></div>
+      <motion.div 
+        className="w-[15rem] bg-(--search-bar-color) h-[2.5rem] rounded-lg" 
+      />
+      <motion.div
+        className="w-full bg-(--search-bar-color) h-[50vh] rounded-lg" 
+      />
     </div>
   )
 }
