75e0da8198fc89c31566a75e002126284c493b33
Roderick Lan <rodericklan03@gmail.com>
2025-09-28 13:14:56 -0600

    cleanup

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -3,11 +3,8 @@ import { LectinContent } from "./lectincontent";
 import LectinContentSkeleton from "./skeleton";
 
 export default async function LectinPage({ params }) {
-  // const { id } = use(params);
-  // const { id } = await params;
-  return (<LectinContentSkeleton />);
   return (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<LectinContentSkeleton />}>
     <LectinContent params={params} />
   </Suspense>
   );
