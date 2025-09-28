b034eae76885f833812da2360ae71d5595e58311
Roderick Lan <rodericklan03@gmail.com>
2025-09-28 12:34:49 -0600

    lectin placeholder skeleton

---
+++ src/app/[id]/skeleton.js
@@ -0,0 +1,9 @@
+
+export default function LectinContentSkeleton() {
+  return (
+    <div className="flex flex-col w-full h-full gap-4">
+      <div className="w-[15rem] bg-(--search-bar-color) h-[2.5rem] rounded-lg" ></div>
+      <div className="w-full bg-(--search-bar-color) rounded-lg h-[50vh]" ></div>
+    </div>
+  )
+}
