b8db56e2ce48002e0b3d5b9651a45730447b9e89
Roderick Lan <rodericklan03@gmail.com>
2025-09-27 20:45:34 -0600

    api prefix

--- app/api/lectin.py
+++ app/api/lectin.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-router.get("/lectin")
+@router.get("/lectin")
 def get_lectin():
   return {"Status": 200}
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # main.py
 from fastapi import FastAPI
+from app.api.router import router
 
 app = FastAPI()
-
+app.include_router(router, prefix="/api")
