b549d08bce246b54905294d51bdd8f5f5418b771
Roderick Lan <rodericklan03@gmail.com>
2025-09-27 21:32:07 -0600

    endpoint update

--- app/api/lectin.py
+++ app/api/lectin.py
@@ -1,10 +1,21 @@
 # api/lectin.py
 from fastapi import APIRouter, Depends
-from typing import Optional
+from app.service.lectin import LectinService
+from app.core.dependencies import get_lectin_service
 router = APIRouter()
 
-@router.get("/lectin")
-def get_lectin(
-  id: Optional[str] = None,
+@router.get("/lectins")
+def get_all_lectins(
+  service: LectinService = Depends(get_lectin_service)
   ):
+  return {
+    "lectins": service.get_lectins()
+  }
+
+@router.get("/lectins/{id}")
+def get_lectin_info(
+  id: str,
+  service: LectinService = Depends(get_lectin_service)
+  ):
+  res = service.get_lectin_info(id)
   return {"Status": 200, "id": id, "type": f'{type(id)}'}
