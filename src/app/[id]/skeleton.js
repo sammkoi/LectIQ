1d5e5eeb0a3b89ee9790e9a0e40a1e1e6279b3c6
Roderick Lan <rodericklan03@gmail.com>
2025-09-28 13:13:50 -0600

    lazy skeleton

--- src/app/[id]/skeleton.js
+++ src/app/[id]/skeleton.js
@@ -1,14 +1,11 @@
-import { motion } from "motion/react";
-
-
 export default function LectinContentSkeleton() {
   return (
     <div className="flex flex-col w-full h-full gap-4">
-      <motion.div 
-        className="w-[15rem] bg-(--search-bar-color) h-[2.5rem] rounded-lg" 
+      <div 
+        className="w-[15rem] bg-(--search-bar-color) h-[2.5rem] rounded-lg animate-pulse"
       />
-      <motion.div
-        className="w-full bg-(--search-bar-color) h-[50vh] rounded-lg" 
+      <div
+        className="w-full bg-(--search-bar-color) h-[50vh] rounded-lg animate-pulse" 
       />
     </div>
   )
