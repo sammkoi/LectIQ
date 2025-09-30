2bab74728f248555fc46ff1d5c2ad793643e5c47
Roderick Lan <rodericklan03@gmail.com>
2025-09-30 13:28:23 -0600

    removed api route

--- src/app/api/lectins/route.js
+++
@@ -1,8 +0,0 @@
-import pl from "nodejs-polars";
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-  // const pl = require("nodejs-polars");
-  // const sheet = pl.read_excel("data/galectins_id_cleaned.xlsx");
-  return NextResponse.json({});
-}
