a99030044ac147b77c8661850511094c2600fa7e
Roderick Lan <rodericklan03@gmail.com>
2025-10-21 19:59:08 -0600

    updated lectin info logic to return unit info and stddev

--- app/service/lectin.py
+++ app/service/lectin.py
@@ -8,12 +8,22 @@ class LectinService:
   data: dict[str, pd.DataFrame]
   
   def __init__(self,):
-    self.data = pd.read_excel("app/data/galectins_id_cleaned.xlsx", sheet_name=None) # todo: dynamic
+    # self.data = pd.read_excel("app/data/galectins_id_cleaned.xlsx", sheet_name=None) # todo: dynamic
+    self.data = pd.read_excel("app/data/Galectins_GlyTouCanID_Updated.xlsx", sheet_name=None) # todo: dynamic
   
   def get_lectin_info(self, id: str) -> Optional[dict]:
     '''Return available glycan info'''
     res = self.data.get(id, None)
     if res is None or res.empty: return None
+    kd_col_name = res.columns[2]
+    unit = kd_col_name.split(',')[1].strip()
+    res = res.rename(columns={
+      kd_col_name: "Kd",
+      "stdev_Kd": "kderr",
+      "SD": "inverr",
+    })
+    res["unit"] = unit
+    
     return res.to_dict(orient="records")
 
   def get_lectins(self) -> list[str]:
